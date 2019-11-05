c104a395a19dd2f06894c24418f57abaa5792b55
Shonpnil <shopnershopnil44@gmail.com>
2019-11-05 20:03:16 +0600

    all fragments created

---
+++ app/src/main/java/com/example/sliinsuredcalculator/fragment/QuarterlyFragment.java
@@ -0,0 +1,32 @@
+package com.example.sliinsuredcalculator.fragment;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.sliinsuredcalculator.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class QuarterlyFragment extends Fragment {
+
+
+    public QuarterlyFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_quarterly, container, false);
+    }
+
+}
